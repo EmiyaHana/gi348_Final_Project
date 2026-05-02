f6198f81995a37332008c800a05b53137810d8c3
EmiyaHana <warm310149@gmail.com>
2026-05-02 08:53:15 +0700

    FinishMap_andGameTest

--- 1670701240/Assets/Scripts/ShadowClawTrap.cs
+++ 1670701240/Assets/Scripts/ShadowClawTrap.cs
@@ -38,6 +38,10 @@ public class ShadowClawTrap : MonoBehaviour
 
     void TryAttackPlayer(GameObject player)
     {
+        if (attackSound != null)
+        {
+            attackSound.Play();
+        }
         DoDamage(player);
     }
 
